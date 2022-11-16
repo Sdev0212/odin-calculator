64e46aa9d5fc1035e403525f1d7bf8e523431ec4
Sdev0212 <seandevine0212@gmail.com>
2022-11-16 16:11:55 +0000

    add logic for user to input numbers that appear on display

--- calculator.js
+++ calculator.js
@@ -10,3 +10,61 @@
     if equals => go back to get operation
     else if operation => go back to get second number
 */
+
+//initialize variables
+let firstNum = ''
+let secondNum = ''
+let operation = null
+
+//buttons and displays
+const numberButtons = document.querySelectorAll(['.number'])
+const operationButtons = document.querySelectorAll(['.operation'])
+const decimalPointButton = document.getElementById('decimal')
+const equalsButton = document.getElementById('equals')
+const clearButton = document.getElementById('clear')
+const deleteButton = document.getElementById('delete')
+const previousDisplay = document.getElementById('previous')
+const currentDisplay = document.getElementById('current')
+
+
+//get numbers and store them for later
+
+numberButtons.forEach((button) => 
+button.addEventListener('click', () => storeNum(button.textContent))
+)
+
+function storeNum(number) {
+    if (currentDisplay.textContent === '0'){
+        resetDisplay()
+    }
+    currentDisplay.textContent += number; 
+}
+
+function resetDisplay(){
+    currentDisplay.textContent = '';
+}
+
+
+
+
+
+
+//operations
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+
