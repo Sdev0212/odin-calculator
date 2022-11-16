b764268641a7f0ade2c48d44a6058294602c77d8
Sdev0212 <seandevine0212@gmail.com>
2022-11-16 14:42:22 +0000

    write pseudocode & set up html

---
+++ calculator.js
@@ -0,0 +1,12 @@
+/* Pseudocode:
+    get first number
+        detect numbers and . store in object & display as typed out
+    get operation
+        detect when operation button pressed
+    get second number
+        detect numbers and . store in object & display as typed
+    wait for equals or another operation
+    do calculation and display result
+    if equals => go back to get operation
+    else if operation => go back to get second number
+*/
