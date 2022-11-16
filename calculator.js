361324e5c177740aaa6ce0049d2af3029855a14c
Sdev0212 <seandevine0212@gmail.com>
2022-11-16 17:28:32 +0000

    fully functional calculator

--- calculator.js
+++ calculator.js
@@ -27,6 +27,12 @@ const previousDisplay = document.getElementById('previous')
 const currentDisplay = document.getElementById('current')
 
 
+//event listeners
+
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+
+
 //get numbers and store them for later
 
 numberButtons.forEach((button) => 
@@ -34,17 +40,63 @@ button.addEventListener('click', () => storeNum(button.textContent))
 )
 
 function storeNum(number) {
-    if (currentDisplay.textContent === '0'){
+    if (currentDisplay.textContent === '0'||shouldReset){
         resetDisplay()
     }
+    currentDisplay.style.fontSize = '50px'
     currentDisplay.textContent += number; 
 }
 
 function resetDisplay(){
     currentDisplay.textContent = '';
+    currentDisplay.style.fontSize = '50px'
+    shouldReset = false;
 }
 
+function clear(){
+    currentDisplay.textContent = '0';
+    previousDisplay.textContent = '';
+    firstNum = '';
+    secondNum = '';
+    operation = null;
+}
 
+//get and set operation
+
+operationButtons.forEach((button) => 
+button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function setOperation(operator) {
+    if(operation !== null) { evaluate()}
+    firstNum = currentDisplay.textContent;
+    operation = operator
+    previousDisplay.textContent = `${firstNum} ${operation}`
+    shouldReset = true
+    
+}
+
+//calculations
+
+function evaluate() {
+    if(operation === null) {return}
+    if(operation === '÷' && currentDisplay.textContent == '0'){
+        currentDisplay.textContent = 'Dividing by 0 is dangerous'
+        currentDisplay.style.fontSize = '20px'
+        shouldReset = true;
+        return;
+    }
+    secondNum = currentDisplay.textContent
+    currentDisplay.textContent = roundResult(
+      calculate(operation, firstNum, secondNum)
+    )
+    previousDisplay.textContent = `${firstNum} ${operation} ${secondNum} =`
+    operation = null
+}
+
+function roundResult(ans) {
+    return Math.round(ans * 1000)/1000;
+}
 
 
 
@@ -67,4 +119,20 @@ function divide(a, b){
     return a / b;
 }
 
-
+function calculate (operation, a, b){
+    a = Number(a);
+    b = Number(b);
+    switch (operation) {
+        case '+':
+          return add(a, b)
+        case '-':
+          return subtract(a, b)
+        case '*':
+          return multiply(a, b)
+        case '÷':
+          if (b === 0) return null
+          else return divide(a, b)
+        default:
+          return null
+    }
+}
